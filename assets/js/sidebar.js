60cd77995c71ab189560d69fa18c01e2d4a3310c
Louttche <lnicolaidou.katerina@gmail.com>
2025-08-04 13:12:03 +0200

    Added social links to sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -3,12 +3,16 @@ function openNav() {
     const sidebarLinks = document.getElementById("nav-sidebar-menu");
     const sidebarOpenBtn = document.getElementById("openbtn");
     const sidebarCloseBtn = document.getElementById("closebtn");
+    const sidebarSocialLinks = document.getElementById("nav-sidebar-social-links");
 
     sidebar.style.width = "200px";
     sidebarOpenBtn.style.display = "none";
     sidebarCloseBtn.style.display = "flex";
     
-    setTimeout(() => sidebarLinks.style.display = "flex", 250);
+    setTimeout(() => {
+        sidebarLinks.style.display = "flex";
+        sidebarSocialLinks.style.display = "flex";
+    }, 250);
 };
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -17,8 +21,10 @@ function closeNav() {
     const sidebarLinks = document.getElementById("nav-sidebar-menu");
     const sidebarOpenBtn = document.getElementById("openbtn");
     const sidebarCloseBtn = document.getElementById("closebtn");
+    const sidebarSocialLinks = document.getElementById("nav-sidebar-social-links");
 
     sidebarCloseBtn.style.display = "none";
+    sidebarSocialLinks.style.display = "none";
     sidebarOpenBtn.style.display = "block";
     sidebarLinks.style.display = "none";
     
