62e923d3e33a3f280558fe4f8dbf0a76141ac0bb
Louttche <lnicolaidou.katerina@gmail.com>
2025-08-11 12:34:36 +0200

    feat: Make contact compact functional

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -1,32 +1,39 @@
+async function validateContactForm(e) {
+  e.preventDefault();
+  const form = e.target;
+
+  if (form.website && form.website.value) {
+    window.location.href = '/contact_confirmation/';
+    return;
+  }
+
+  const data = new FormData(form);
+  const action = form.action;
+  try {
+    const response = await fetch(action, {
+      method: 'POST',
+      body: data,
+      headers: { 'Accept': 'application/json' }
+    });
+    if (response.ok) {
+      window.location.href = '/contact_confirmation/';
+    } else {
+      alert('There was a problem submitting your form.');
+    }
+  } catch (error) {
+    alert('There was a problem submitting your form.');
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const contactForm = document.getElementById('contact-form')
+  const contactForm = document.getElementById('contact-form');
+  const contactCompactForm = document.getElementById('contact-form-compact');
   
   if (contactForm != null) {
-    contactForm.addEventListener('submit', async function(e) {
-      e.preventDefault();
-      const form = e.target;
-  
-      if (form.website && form.website.value) {
-        window.location.href = '/contact_confirmation/';
-        return;
-      }
-  
-      const data = new FormData(form);
-      const action = form.action;
-      try {
-        const response = await fetch(action, {
-          method: 'POST',
-          body: data,
-          headers: { 'Accept': 'application/json' }
-        });
-        if (response.ok) {
-          window.location.href = '/contact_confirmation/';
-        } else {
-          alert('There was a problem submitting your form.');
-        }
-      } catch (error) {
-        alert('There was a problem submitting your form.');
-      }
-    });
+    contactForm.addEventListener('submit', (e) => validateContactForm(e));
+  }
+
+  if (contactCompactForm != null) {
+    contactCompactForm.addEventListener('submit', (e) => validateContactForm(e));
   }
 });
