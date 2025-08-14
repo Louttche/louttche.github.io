36e2eacd87f1785cca1bdf4541d36c34fc5c5c83
Louttche <lnicolaidou.katerina@gmail.com>
2025-08-14 16:12:04 +0200

    refactor: Make active nav link check more readable

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,28 +10,13 @@ const isElementXPercentInViewport = function(el, percentVisible = 60) {
   )
 };
 
-const getVisibleSectionId = () => {
-    const aboutMeSection = document.getElementById('about-me');
-    const projectsSection = document.getElementById('projects');
-    const skillsSection = document.getElementById('skills');
-    const contactSection = document.getElementById('contact');
-
-    if (isElementXPercentInViewport(aboutMeSection)) {
-        return 'about-me';
-    } else if (isElementXPercentInViewport(projectsSection)) {
-        return 'projects';
-    } else if (isElementXPercentInViewport(skillsSection)) {
-        return 'skills';
-    } else if (isElementXPercentInViewport(contactSection)) {
-        return 'contact';
-    }
-};
-
 const setActiveNavLink = () => {
     const navLinks = document.getElementsByClassName("nav-link");
 
     for (let navLink of navLinks) {
-        if (navLink.innerHTML.toLowerCase().replace(' ', '-') == getVisibleSectionId()) {
+        const sectionID = navLink.innerHTML.toLowerCase().replace(' ', '-');
+
+        if (isElementXPercentInViewport(document.getElementById(sectionID))) {
             navLink.classList.add("active");
         } else {
             navLink.classList.remove("active");
@@ -39,23 +24,23 @@ const setActiveNavLink = () => {
     }
 };
 
+const setDocumentOverflowY = function (mode) {
+    document.documentElement.style.overflowY = mode;
+    document.body.style.overflowY = mode;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const scrollContainer = document.querySelector(".scroll-container");
     const homePage = document.getElementById("home-page-content");
 
     if (homePage != null) {
-        // Nav links - set as active based on scroll event
         setActiveNavLink();
         scrollContainer.addEventListener("scroll", (_e) => {
             setActiveNavLink();
         });
 
-        // Overflow snap behaviour fix (disable scrolling on `html, body` on homepage to only use the snap scrolling)
-        document.documentElement.style.overflowY = 'hidden';
-        document.body.style.overflowY = 'hidden';
+        setDocumentOverflowY('hidden');
     } else {
-        // Enable scrolling on `html, body` for the pages without snap-scrolling (hard-coded homepage)
-        document.documentElement.style.overflowY = 'auto';
-        document.body.style.overflowY = 'auto';
+        setDocumentOverflowY('auto');
     }
 });
